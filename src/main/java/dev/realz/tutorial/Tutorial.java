7658520814c95b882324ceec63441eb976ad489c
kingrealzyt <jonathansanie@gmail.com>
2022-07-24 15:28:17 -0500

    Initial Commit

---
+++ src/main/java/dev/realz/tutorial/Tutorial.java
@@ -0,0 +1,13 @@
+package dev.realz.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tutorial implements ModInitializer {
+
+    public static final String MOD_ID = "tm";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
